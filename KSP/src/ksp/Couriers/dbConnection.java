9fcdb5c82a6480e54cc7fea6ad5bb77561633f35
marrut <martynas.rutkus@gmail.com>
2018-12-11 00:00:00 +0200

    padarytas prekiu salinimas

--- KSP/src/ksp/Couriers/dbConnection.java
+++ KSP/src/ksp/Couriers/dbConnection.java
@@ -4,131 +4,163 @@
  * and open the template in the editor.
  */
 package ksp.Couriers;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 /**
  *
  * @author Admin
  */
 public class dbConnection {
-  private String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
-  private String MYSQL_URL = "jdbc:mysql://localhost:3306/ksp";
-  private String MYSQL_USER = "root";
-  private String MYSQL_PASSWD = "";
-  private Connection con;
-  private Statement st;
-  private ResultSet rs;
-public  dbConnection() {
-    try {
-      Class.forName(MYSQL_DRIVER);
-      System.out.println("Class Loaded....");
-      con = DriverManager.getConnection(MYSQL_URL,MYSQL_USER,MYSQL_PASSWD);
-      System.out.println("Connected to the database....");
-      st = con.createStatement();
-    } catch(ClassNotFoundException ex) {
-       System.out.println("ClassNotFoundException:\n"+ex.toString());
-       ex.printStackTrace();
 
-    } catch(SQLException ex) {
-        System.out.println("SQLException:\n"+ex.toString());
-        ex.printStackTrace();
-    }
-  }  
-public ResultSet getCouriersList()
-{
-    try{
-         rs = st.executeQuery("SELECT * FROM kurjeriai");    
-    } catch(SQLException ex){
-        System.out.println("SQLException:\n"+ex.toString());
-        ex.printStackTrace();
-    }
-   return rs;
-}
-public ResultSet getRoutes()
-{
-    try{
-         rs = st.executeQuery("SELECT * FROM marsrutai");    
-    } catch(SQLException ex){
-        System.out.println("SQLException:\n"+ex.toString());
-        ex.printStackTrace();
-    }
-   return rs;
-}
+    private String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+    private String MYSQL_URL = "jdbc:mysql://localhost:3306/ksp";
+    private String MYSQL_USER = "root";
+    private String MYSQL_PASSWD = "";
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
 
-public ResultSet getOrderedProducts()
-{
-    try{
-         rs = st.executeQuery("SELECT * FROM uzsakytos_prekes");    
-    } catch(SQLException ex){
-        System.out.println("SQLException:\n"+ex.toString());
-        ex.printStackTrace();
-    }
-   return rs;
-}
+    public dbConnection() {
+        try {
+            Class.forName(MYSQL_DRIVER);
+            System.out.println("Class Loaded....");
+            con = DriverManager.getConnection(MYSQL_URL, MYSQL_USER, MYSQL_PASSWD);
+            System.out.println("Connected to the database....");
+            st = con.createStatement();
+        } catch (ClassNotFoundException ex) {
+            System.out.println("ClassNotFoundException:\n" + ex.toString());
+            ex.printStackTrace();
 
-public ResultSet getOrderedProductsByCarID(String carID)
-{
-    try{
-         rs = st.executeQuery("SELECT * FROM uzsakytos_prekes WHERE fk_automobilis='"+carID+"';");    
-    } catch(SQLException ex){
-        System.out.println("SQLException:\n"+ex.toString());
-        ex.printStackTrace();
+        } catch (SQLException ex) {
+            System.out.println("SQLException:\n" + ex.toString());
+            ex.printStackTrace();
+        }
     }
-   return rs;
-}
-public ResultSet getCarsList()
-{
-    try{
-         rs = st.executeQuery("SELECT * FROM automobiliai");    
-    } catch(SQLException ex){
-        System.out.println("SQLException:\n"+ex.toString());
-        ex.printStackTrace();
-    }
-   return rs;
-}
-public boolean executeUpdate(String query){
-    try{
-    st.executeUpdate(query);
-    }catch(SQLException ex){
-        System.out.println("SQLException:\n"+ex.toString());
-        ex.printStackTrace();
-        return false;
-    }
-    return true;
-}
 
-public ResultSet getCategories() {
-    try {
-        rs = st.executeQuery("SELECT * FROM kategorijos");
-    } catch(SQLException e) {
-        System.out.println("'SQLException:\n" + e.toString());
-        e.printStackTrace();
+    public ResultSet getCouriersList() {
+        try {
+            rs = st.executeQuery("SELECT * FROM kurjeriai");
+        } catch (SQLException ex) {
+            System.out.println("SQLException:\n" + ex.toString());
+            ex.printStackTrace();
+        }
+        return rs;
     }
-    return rs;
-}
 
-public ResultSet getWarehouses() {
-    try {
-        rs = st.executeQuery("SELECT * FROM sandeliai");
-    } catch(SQLException e) {
-        System.out.println("'SQLException:\n" + e.toString());
-        e.printStackTrace();
+    public ResultSet getRoutes() {
+        try {
+            rs = st.executeQuery("SELECT * FROM marsrutai");
+        } catch (SQLException ex) {
+            System.out.println("SQLException:\n" + ex.toString());
+            ex.printStackTrace();
+        }
+        return rs;
     }
-    return rs;
-}
 
-public ResultSet getProducts() {
-    try {
-        rs = st.executeQuery("SELECT * FROM prekes");
-    } catch(SQLException e) {
-        System.out.println("'SQLException:\n" + e.toString());
-        e.printStackTrace();
+    public ResultSet getOrderedProducts() {
+        try {
+            rs = st.executeQuery("SELECT * FROM uzsakytos_prekes");
+        } catch (SQLException ex) {
+            System.out.println("SQLException:\n" + ex.toString());
+            ex.printStackTrace();
+        }
+        return rs;
     }
-    return rs;
-}
 
+    public ResultSet getOrderedProductsByCarID(String carID) {
+        try {
+            rs = st.executeQuery("SELECT * FROM uzsakytos_prekes WHERE fk_automobilis='" + carID + "';");
+        } catch (SQLException ex) {
+            System.out.println("SQLException:\n" + ex.toString());
+            ex.printStackTrace();
+        }
+        return rs;
+    }
+
+    public ResultSet getCarsList() {
+        try {
+            rs = st.executeQuery("SELECT * FROM automobiliai");
+        } catch (SQLException ex) {
+            System.out.println("SQLException:\n" + ex.toString());
+            ex.printStackTrace();
+        }
+        return rs;
+    }
+
+    public boolean executeUpdate(String query) {
+        try {
+            st.executeUpdate(query);
+        } catch (SQLException ex) {
+            System.out.println("SQLException:\n" + ex.toString());
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public ResultSet getCategories() {
+        try {
+            rs = st.executeQuery("SELECT * FROM kategorijos");
+        } catch (SQLException e) {
+            System.out.println("'SQLException:\n" + e.toString());
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    public ResultSet getWarehouses() {
+        try {
+            rs = st.executeQuery("SELECT * FROM sandeliai");
+        } catch (SQLException e) {
+            System.out.println("'SQLException:\n" + e.toString());
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    public ResultSet getWarehouses(String product) {
+        try {
+            rs = st.executeQuery(String.format("SELECT * FROM sandeliai INNER JOIN sandelio_prekes ON sandelio_prekes.sandelio_id=sandeliai.id WHERE sandelio_prekes.prekes_kodas=\'%s\'", product));
+        } catch (SQLException e) {
+            System.out.println("'SQLException:\n" + e.toString());
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    public ResultSet getProducts() {
+        try {
+            rs = st.executeQuery("SELECT * FROM prekes");
+        } catch (SQLException e) {
+            System.out.println("'SQLException:\n" + e.toString());
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    public ResultSet getProducts(int warehouse) {
+        try {
+            rs = st.executeQuery(String.format("SELECT * FROM prekes INNER JOIN sandelio_prekes ON sandelio_prekes.prekes_kodas=prekes.kodas WHERE sandelio_prekes.sandelio_id=%d", warehouse));
+        } catch (SQLException e) {
+            System.out.println("'SQLException:\n" + e.toString());
+            e.printStackTrace();
+        }
+        return rs;
+    }
+    
+    public ResultSet getProductInWarehouse(int warehouse, String product) {
+        try {
+            rs = st.executeQuery(String.format("SELECT * FROM sandelio_prekes WHERE sandelio_id=%d AND prekes_kodas=\'%s\' ", warehouse, product));
+        } catch (SQLException e) {
+            System.out.println("'SQLException:\n" + e.toString());
+            e.printStackTrace();
+        }
+        return rs;
+    }
 
 }
--- KSP/src/ksp/sandeliavimas/PrekiuSalinimasController.java
+++ KSP/src/ksp/sandeliavimas/PrekiuSalinimasController.java
@@ -6,13 +6,17 @@
 package ksp.sandeliavimas;
 
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import ksp.Couriers.dbConnection;
 import ksp.LoginManager;
 import ksp.MainViewManager;
 
@@ -23,6 +27,7 @@ import ksp.MainViewManager;
  */
 public class PrekiuSalinimasController implements Initializable {
 
+    dbConnection dbc = new dbConnection();
     @FXML
     private Button logoutButton;
     @FXML
@@ -35,6 +40,12 @@ public class PrekiuSalinimasController implements Initializable {
     private TextField count;
     @FXML
     private Button delete;
+    @FXML
+    private ComboBox product;
+    @FXML
+    private ComboBox warehouse;
+    @FXML
+    private Label message;
 
     /**
      * Initializes the controller class.
@@ -42,7 +53,7 @@ public class PrekiuSalinimasController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     void initView(SandeliavimasManager sandeliavimas, MainViewManager mainManager, LoginManager loginManager, String sessionID) {
         sessionLabel.setText(sessionID);
@@ -53,8 +64,166 @@ public class PrekiuSalinimasController implements Initializable {
             loginManager.authenticated(sessionID);
         });
         backButton.setOnAction((ActionEvent event) -> {
-           mainManager.navigateStorage(loginManager, sessionID);
+            mainManager.navigateStorage(loginManager, sessionID);
         });
+
+        ResultSet products = dbc.getProducts();
+        try {
+            while (products.next()) {
+                product.getItems().add(products.getString("kodas") + " " + products.getString("pavadinimas"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("ERROR: " + ex);
+            ex.printStackTrace();
+        }
+
+        ResultSet warehouses = dbc.getWarehouses();
+        try {
+            while (warehouses.next()) {
+                warehouse.getItems().add(warehouses.getString("adresas") + ", " + warehouses.getString("miestas"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("ERROR: " + ex);
+            ex.printStackTrace();
+        }
     }
-    
+
+    @FXML
+    public void addButton(ActionEvent event) {
+        if (product.getValue() != null && warehouse.getValue() != null
+                && count.getText() != null) {
+            int count = Integer.parseInt(this.count.getText());
+            String product = this.product.getValue().toString();
+            String warehouse = this.warehouse.getValue().toString();
+
+            ResultSet warehouses = dbc.getWarehouses();
+            int warehouseId = 0;
+            try {
+                while (warehouses.next()) {
+                    String w = warehouses.getString("adresas") + ", " + warehouses.getString("miestas");
+                    if (w.equals(warehouse)) {
+                        warehouseId = warehouses.getInt("id");
+                    }
+                }
+            } catch (SQLException ex) {
+                System.out.println("ERROR: " + ex);
+                ex.printStackTrace();
+            }
+
+            String productCode = "";
+            ResultSet products = dbc.getProducts();
+            try {
+                while (products.next()) {
+                    String p = products.getString("kodas") + " " + products.getString("pavadinimas");
+                    if (p.equals(product)) {
+                        productCode = products.getString("kodas");
+                    }
+                }
+            } catch (SQLException ex) {
+                System.out.println("ERROR: " + ex);
+                ex.printStackTrace();
+            }
+
+            ResultSet pw = dbc.getProductInWarehouse(warehouseId, productCode);
+            int id = -1;
+            int overallCount = -1;
+            try {
+                while (pw.next()) {
+                    id = pw.getInt("id");
+                    overallCount = pw.getInt("kiekis");
+                    break;
+                }
+            } catch (SQLException ex) {
+                System.out.println("ERROR: " + ex);
+                ex.printStackTrace();
+            }
+
+            String query;
+            boolean ok;
+            if (overallCount - count > 0) {
+                query = String.format("UPDATE sandelio_prekes SET kiekis=%d WHERE id=%d", overallCount - count, id);
+                System.out.println("Sending query: " + query);
+                ok = dbc.executeUpdate(query);
+                if (ok) {
+                    message.setText("Sumažintas sandėlio prekės kiekis.");
+                } else {
+                    message.setText("Nepavyko sumažinti sandėlio prekės kiekio.");
+                }
+            } else {
+                query = String.format("DELETE FROM sandelio_prekes WHERE id=%d", id);
+                System.out.println("Sending query: " + query);
+                ok = dbc.executeUpdate(query);
+                if (ok) {
+                    message.setText("Ištrintas sandėlio prekės įrašas.");
+                } else {
+                    message.setText("Nepavyko ištrinti sandėlio prekės įrašo.");
+                }
+            }
+        } else {
+            message.setText("Prašome užpildyti visus laukelius.");
+        }
+    }
+
+    @FXML
+    public void selectProduct(ActionEvent event) {
+        if (warehouse.getValue() == null) {
+            warehouse.getItems().clear();
+            String product = this.product.getValue().toString();
+            String productCode = "";
+            ResultSet products = dbc.getProducts();
+            try {
+                while (products.next()) {
+                    String p = products.getString("kodas") + " " + products.getString("pavadinimas");
+                    if (p.equals(product)) {
+                        productCode = products.getString("kodas");
+                    }
+                }
+            } catch (SQLException ex) {
+                System.out.println("ERROR: " + ex);
+                ex.printStackTrace();
+            }
+
+            ResultSet warehouses = dbc.getWarehouses(productCode);
+            try {
+                while (warehouses.next()) {
+                    warehouse.getItems().add(warehouses.getString("adresas") + ", " + warehouses.getString("miestas"));
+                }
+            } catch (SQLException ex) {
+                System.out.println("ERROR: " + ex);
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    public void selectWarehouse(ActionEvent event) {
+        if (product.getValue() == null) {
+            product.getItems().clear();
+            String warehouse = this.warehouse.getValue().toString();
+            ResultSet warehouses = dbc.getWarehouses();
+            int warehouseId = 0;
+            try {
+                while (warehouses.next()) {
+                    String w = warehouses.getString("adresas") + ", " + warehouses.getString("miestas");
+                    if (w.equals(warehouse)) {
+                        warehouseId = warehouses.getInt("id");
+                    }
+                }
+            } catch (SQLException ex) {
+                System.out.println("ERROR: " + ex);
+                ex.printStackTrace();
+            }
+
+            ResultSet products = dbc.getProducts(warehouseId);
+            try {
+                while (products.next()) {
+                    product.getItems().add(products.getString("kodas") + " " + products.getString("pavadinimas"));
+                }
+            } catch (SQLException ex) {
+                System.out.println("ERROR: " + ex);
+                ex.printStackTrace();
+            }
+        }
+    }
+
 }
